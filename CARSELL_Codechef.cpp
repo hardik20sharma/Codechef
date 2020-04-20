32e416620e55d5e2927811dcd86280804a39de5b
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-04-20 18:30:07 +0530

    Add files via upload

---
+++ CARSELL_Codechef.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        int a[n];
+        unsigned long long int sum = 0;
+
+        for(int i=0; i<n; i++)
+            cin >> a[i];
+        
+        sort(a, a+n);
+
+        for(int i=0; i<n; i++)
+        {
+            if(a[n-i-1] - i < 0)
+                continue;
+            sum += a[n-i-1] - i;
+        }
+
+        cout << sum % 1000000007 << "\n";
+    }
+}

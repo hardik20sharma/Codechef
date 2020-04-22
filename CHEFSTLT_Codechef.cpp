103abc43d8d538eb292ad2f93dfab3fb5e7934e3
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-04-22 18:59:04 +0530

    Add files via upload

---
+++ CHEFSTLT_Codechef.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t; 
+    cin >> t;
+    while(t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+        int min = 0, max = 0;
+        for(int i=0; i<s1.length(); i++)
+        {
+            if(s1[i]=='?' || s2[i]=='?' || s1[i]!=s2[i])
+                max++;
+            
+            if(s1[i]!='?' && s2[i]!='?' && s1[i]!=s2[i])
+                min++;
+        }
+        cout << min << " " << max << "\n";
+    }
+}

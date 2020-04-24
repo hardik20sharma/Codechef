776116ed33197dd348f807d06a5309eae51d6daf
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-04-24 21:56:17 +0530

    Add files via upload

---
+++ COVIDLQ_Codechef.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        
+        int a[n];
+        vector<int> store;
+        for(int i=0; i<n; i++)
+        {
+            cin >> a[i];
+            if(a[i] == 1)
+                store.push_back(i);
+        }
+
+        bool ans = true;
+        for(int i=1; i<store.size(); i++)
+            if(store[i] - store[i-1] < 6)
+                ans = false;
+
+        ans ? cout << "YES\n" : cout << "NO\n";
+    }
+}

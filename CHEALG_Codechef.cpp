bf171b86ebb912f7b03911c014cb2416e2efbd8b
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-04-25 19:19:45 +0530

    Add files via upload

---
+++ CHEALG_Codechef.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string str, final_str="";
+        cin >> str;
+
+        for(int i=0; i<str.length(); i++)
+        {
+            char c = str[i];
+            int cnt = 1;
+            while(c==str[i+1])
+            {
+                cnt++;
+                i++;
+            }
+            final_str += (c + to_string(cnt));
+        }
+        (final_str.length() < str.length()) ? cout << "YES\n" : cout << "NO\n";
+    }
+}

ba84f1cdbd70626a1e1dbd96d507464773f4990a
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-04-27 10:51:46 +0530

    Add files via upload

---
+++ CV_Codechef.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, cnt = 0;
+        string str;
+        cin >> n >> str;
+
+        for(int i=0; i<n-1; i++)
+        {
+            bool cnd1 = (str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u');
+            bool cnd2 = (str[i+1]=='a' || str[i+1]=='e' || str[i+1]=='i' || str[i+1]=='o' || str[i+1]=='u');
+            if(cnd1 && cnd2)
+                cnt++;
+        }
+        cout << cnt << "\n";
+    }
+}

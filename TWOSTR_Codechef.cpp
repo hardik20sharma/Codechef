fee4c9338769ce8623393399acdc7a4cccf4142a
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-04-30 18:24:01 +0530

    Add files via upload

---
+++ TWOSTR_Codechef.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool func(string s1, string s2)
+{
+    for(int i=0; i<s1.length(); i++)
+            if(s1[i]!='?' && s2[i]!='?')
+                if(s1[i]!=s2[i])
+                    return false;
+    return true;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+        func(s1, s2) ? cout << "Yes\n" : cout << "No\n";
+    }
+}

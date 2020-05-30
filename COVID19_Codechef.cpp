a5113c4f707fbc38a14481484db208bc37268204
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-05-30 19:37:12 +0530

    Add files via upload

---
+++ COVID19_Codechef.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, min=10, max=-1;
+        cin >> n;
+
+        vector<int> vec(n);
+        for(int i=0; i<n; i++)
+            cin >> vec[i];
+
+        for(int i=0; i<n; i++)
+        {
+            int j=i+1, cnt=1, temp = i;
+            while(j<n && vec[j++]-vec[temp++] <=2)
+                cnt++;
+
+            j=i-1, temp = i;
+            while(0 <= j && vec[temp--]-vec[j--] <=2)
+                cnt++;
+
+            if(cnt < min)
+                min = cnt;
+            else if(max < cnt)
+                max = cnt;
+        }
+        cout << min << " " << max << "\n";
+    }
+}

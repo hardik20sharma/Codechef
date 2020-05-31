aea5e4103a1a719375bbe3b832190bb269e69772
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-05-31 10:08:12 +0530

    Add files via upload

---
+++ TWTCLOSE_Codechef.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> v;
+    for(int i=0; i<k; i++)
+    {
+        string s;
+        cin >> s;
+        if(s=="CLICK")
+        {
+            int n;
+            cin >> n;
+            if(find(v.begin(), v.end(), n) == v.end())
+                v.push_back(n);
+            else
+                v.erase(find(v.begin(), v.end(), n));
+        }
+        else
+            v.clear();
+        cout << v.size() << "\n";
+    }
+}

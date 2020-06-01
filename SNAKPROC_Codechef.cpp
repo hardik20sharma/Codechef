8b1bdaef31239a9dd0e48ad4ad4069c324194181
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-06-01 10:10:10 +0530

    Add files via upload

---
+++ SNAKPROC_Codechef.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool func(int n, string str)
+{
+    int step = 0;
+    for(int i=0; i<n; i++)
+    {
+        if(str[i]=='H')
+            step++;
+        else if(str[i]=='T')
+            step--;
+        
+        if(step !=0 && step != 1)
+            return false;
+    }
+    return !step;
+}
+
+int main()
+{
+    int t, n;
+    cin >> t;
+    while(t--)
+    {
+        string str;
+        cin >> n >> str;
+        func(n, str) ? cout << "Valid\n" : cout << "Invalid\n";
+    }
+}

c36b86250e06abe29690dc534991311a10b41f1a
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-06-10 08:56:47 +0530

    Add files via upload

---
+++ XYSTR_Codechef.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unsigned short t, cnt;
+    cin >> t;
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        cnt = 0;
+
+        for(int i=0; i<s.length()-1; i++)
+            if( (s[i]=='x' && s[i+1]=='y') || (s[i]=='y' && s[i+1]=='x') )
+            {
+                i++;
+                cnt++;
+            }
+        cout << cnt << "\n";
+    }
+}

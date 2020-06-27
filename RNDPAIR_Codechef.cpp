c1c310031a431bc3380cc2a7b0348ae85d056157
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-06-27 21:59:33 +0530

    Add files via upload

---
+++ RNDPAIR_Codechef.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, max = -1;
+        cin >> n;
+
+        double cnt=0, total_possibilities = (n*(n-1))/2;
+
+        vector<int> a(n);
+        for(int i=0; i<n; i++)
+            cin >> a[i];
+
+        for(int i=0; i<n; i++)
+            for(int j=i+1; j<n; j++)
+                if(a[i] + a[j] > max)
+                {
+                    max = a[i] + a[j];
+                    cnt = 1;
+                }
+                else if(a[i] + a[j] == max)
+                    cnt++;
+
+        cout << setprecision(8) << cnt/total_possibilities << "\n";
+    }
+}

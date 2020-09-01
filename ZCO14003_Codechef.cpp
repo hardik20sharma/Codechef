127d406fc3a8f44ed9fffd9c69fc3d9823db885f
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-01 15:20:11 +0530

    C++ code

--- ZCO14003_Codechef.cpp
+++ ZCO14003_Codechef.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
 
 int main()
 {
-    ll n;
-    cin >> n;
-    
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    ll n; cin >> n;
+
     ll a[n];
     for(ll i=0; i<n; i++)
         cin >> a[i];
-    
+
     sort(a, a+n);
 
     ll prev = 0, next = 0;

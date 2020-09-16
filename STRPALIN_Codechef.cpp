28236f4921ca2b9df5a11abf832ee268adcd0e6e
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-16 18:35:59 +0530

    Add files via upload

---
+++ STRPALIN_Codechef.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool func(string s1, string s2)
+{
+    for(int i=0; i<s2.length(); i++)
+        if(find(s1.begin(), s1.end(), s2[i]) != s1.end())
+            return true;
+    return false;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;        
+        func(s1, s2) ? cout << "Yes\n" : cout << "No\n";
+    }
+}

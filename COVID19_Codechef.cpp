6c8c3aba5793980e3aae9c94882bce75af54aaed
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-09-18 14:28:43 +0530

    C++ code

--- COVID19_Codechef.cpp
+++ COVID19_Codechef.cpp
@@ -9,7 +9,7 @@ int main()
     cin >> t;
     while(t--)
     {
-        int n, min=10, max=-1;
+        int n, min = 10, max = -1;
         cin >> n;
 
         vector<int> vec(n);
@@ -18,11 +18,11 @@ int main()
 
         for(int i=0; i<n; i++)
         {
-            int j=i+1, cnt=1, temp = i;
+            int j = i+1, cnt = 1, temp = i;
             while(j<n && vec[j++] - vec[temp++] <= 2)
                 cnt++;
 
-            j=i-1, temp = i;
+            j = i-1, temp = i;
             while(0 <= j && vec[temp--] - vec[j--] <= 2)
                 cnt++;
 

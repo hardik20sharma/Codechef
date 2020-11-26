970e47f1bc07902a9e37ce99dee340ff0b116851
Hardik Sharma <47312506+hardik20sharma@users.noreply.github.com>
2020-11-26 21:38:02 +0530

    Update FRUITS_Codechef.cpp

--- FRUITS_Codechef.cpp
+++ FRUITS_Codechef.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int t;
-    cin >> t;
+    int t; cin >> t;
     while(t--)
     {
         int m, n, k;
